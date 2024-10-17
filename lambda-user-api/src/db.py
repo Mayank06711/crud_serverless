f23a8d4b22795248c77cead98949b905d05a64c9
Mayank Soni <mayank93soni@gmail.com>
2024-10-17 15:55:30 +0530

    hey codebase , change db function to use environment varible

--- lambda-user-api/src/db.py
+++ lambda-user-api/src/db.py
@@ -1,18 +1,22 @@
+import os
 import psycopg2
-
-DB_HOST = "host endpoint"
-DB_NAME = "name"
-DB_USER = "postgres"
-DB_PASSWORD = "mypassword"
 
 def get_db_connection():
     try:
+        # Fetch credentials from environment variables that we will be setting in serverless.yaml file
+        DB_HOST = os.getenv('DB_HOST')
+        DB_NAME = os.getenv('DB_NAME')
+        DB_USER = os.getenv('DB_USER')
+        DB_PASSWORD = os.getenv('DB_PASSWORD')
+        DB_PORT = os.getenv('DB_PORT', '5432')
+
+        # Create a connection
         connection = psycopg2.connect(
             host=DB_HOST,
             database=DB_NAME,
-            user=DB_USER, #master username on aws
-            password=DB_PASSWORD, #db password
-            port="5432", #db port 
+            user=DB_USER,
+            password=DB_PASSWORD,
+            port=DB_PORT
         )
         print("Connected to RDS (PostgreSQL) successfully!")
         return connection
